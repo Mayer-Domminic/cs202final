4218498e96841b9817fe3092075a3fdac9e86ded
finnenicholas <nfinne@nevada.urn.edu>
2022-05-04 20:11:44 -0700

    iWavModel added onto

--- iWavModel.cpp
+++ iWavModel.cpp
@@ -2,54 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
-//iWavModel::iWavModel(){}
 
-//virtual iWavModel::~iWavModel(){}
-/*virtual FileAttributes iWaveModel::openFile(const std::string &file_name){
-	ifstream file(file_name, ios::binary | ios::in);
+FileAttributes openFile(const string &filename){
+	ifstream file(filename, ios::binary | ios::in);
+	short* buffer = nullptr;
+
 	if(file.is_open()){
-		file.read((char *) &wavHeader, sizeof(wav_header));
-		buffer = new short[waveHeader.data_bytes];
-		file.read((char*) buffer, waveHeader.data_bytes);
 
-		for (int i = 0; i < waveHeader.data_bytes / waveHeader.sample_alignment; i++){
-			soundeData.push_back((float)buffer[i] / MAX_16BIT);
+		file.read((char*)&header, 20);
+		file.read((char*)&header + 20, header.subchunk1_size);
+		file.read((char*)&header + 20 + header.subchunk1_size, 8);
+
+		buffer = new short[header.data_bytes];
+		file.read((char*)buffer, header.data_bytes);
+
+		int maxSize = pow(2, header.bits_per_sample - 1);
+		int upperBound = header.data_bytes / header.block_align * header.num_channels;
+
+		if(header.num_channels == 1){
+			for(int i = 0; i < upperBound; i++){
+				data[0].push_back((float)buffer[i] / maxSize); //second arg in power is max value of the bitrate
+			}
+		} else {
+			data.push_back({});
+			for(int i = 0; i < upperBound; i++){
+				if(i % 2 == 0) {
+					data[0].push_back((float)buffer[i] / maxSize); //left ear bit 1
+				} else {
+					data[1].push_back((float)buffer[i] / maxSize); //right ear bit 1
+				}
+			}
 		}
 		file.close();
-	}	
-	else{
-		cout << "not able to read file" << endl;
-		//return 0;
 	}
 	delete[] buffer;
-}*/
-
-virtual FileAt iWaveModel::readFile(const std::string &file_name){
-	int read;
-	unsigned char buffer4[4];
-	FILE* file = fopen(file_name, "rb");
-
-	if(file == NULL){
-		cout << "not able to open file" << endl;
-		return 0;
-	} else{
-		FILE * outputfile = fopen("Echoed_output.wav", "wb");
-		//Reads header of file
-		size_t bytesRead = fread(&wavHeader, 1, headerSize, file);
-		fwrite(&wavHeader, 1, headerSize, outputfile);
-
-		//Reads data
-		if(bytesRead > 0){
-			sample init_samples[wavHeader.sample_rate];
-			long size_of_each_sample = (wavHeader.num_channels * wavHeader.bit_depth);
-			read = fread(buffer4, sizeof(buffer4), 1, file);
-			unsigned int data_size = buffer4[0] | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
-
-			long num_samples = (8 * wavHeader.data_bytes) / (waveHeader.num_channels * wavHeader.bit_depth);
-		}
-	}
-	
 }
-virtual std::vector<std::string> getProcessList()=0;
-virtual void setOutPutFileName(const std::string& outputFileName)=0;
-virtual void processFile(const std::string& processID)=0;
--- iWavModel.h
+++ iWavModel.h
@@ -2,16 +2,24 @@
 #define FEED_BACK_ECHO_IWAVEMODEL_H
 
 #include "FileAttributes.h"
+#include "Console.h"
 #include <vector>
 #include <string>
 using namespace std;
 
 class iWavModel {
+private:
+	FileAttributes attr;
+	string outputFilename = console -> getOutputFileName();
 public:
+	/*
 	virtual FileAttributes openFile(const std::string &file_name)=0;
 	virtual vector<string> getProcessList()=0;
 	virtual void setOutPutFileName(const std::string& outputFileName)=0;
 	virtual void processFile(const std::string& processID)=0;
+	*/
+
+	FileAttributes openFile(const string &filename)=0;
 };
 
 #endif //FEED_BACK_ECHO_IWAVEMODEL_H
